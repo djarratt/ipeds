e06309d12b231f600f18ed2833776ba430557dfa
Daniel Jarratt <daniel.jarratt@infinitecampus.com>
2016-09-09 13:49:11 -0500

    final two of six Carnegie classifications

--- IPEDS-WD.R
+++ IPEDS-WD.R
@@ -18,14 +18,32 @@ crosswalked = merge(schools, unitid.qid, by.x = 'unitid', by.y = 'unitid')
 #  select(schoolQid = qid, carnegieQid = wikidataQid)
 #cat(paste(basic$schoolQid, 'P2643', basic$carnegieQid, 'S248','Q4223026',sep = '\t'), sep = '\n')
 
+#enrollment.profile = merge(crosswalked, 
+#                           carnegie.qid %>% filter(carnegieClassificationType == 'Enrollment Profile'),
+#                           by.x = 'Enrollment.Profile', by.y = 'carnegieClassification') %>%
+#  select(schoolQid = qid, carnegieQid = wikidataQid)
+#cat(paste(enrollment.profile$schoolQid, 'P2643', enrollment.profile$carnegieQid, 'S248','Q4223026',sep = '\t'), sep = '\n')
+
 #grad.program = merge(crosswalked, 
 #                     carnegie.qid %>% filter(carnegieClassificationType == 'Graduate Instructional Program'),
 #                     by.x = 'Graduate.Program', by.y = 'carnegieClassification') %>%
 #  select(schoolQid = qid, carnegieQid = wikidataQid)
 #cat(paste(grad.program$schoolQid, 'P2643', grad.program$carnegieQid, 'S248','Q4223026',sep = '\t'), sep = '\n')
 
-size.setting = merge(crosswalked, 
-                     carnegie.qid %>% filter(carnegieClassificationType == 'Size and Setting'),
-                     by.x = 'Size...Setting', by.y = 'carnegieClassification') %>%
+#size.setting = merge(crosswalked, 
+#                     carnegie.qid %>% filter(carnegieClassificationType == 'Size and Setting'),
+#                     by.x = 'Size...Setting', by.y = 'carnegieClassification') %>%
+#  select(schoolQid = qid, carnegieQid = wikidataQid)
+#cat(paste(size.setting$schoolQid, 'P2643', size.setting$carnegieQid, 'S248','Q4223026',sep = '\t'), sep = '\n')
+
+# undergraduate.profile = merge(crosswalked, 
+#                               carnegie.qid %>% filter(carnegieClassificationType == 'Undergraduate Profile'),
+#                               by.x = 'Undergraduate.Profile', by.y = 'carnegieClassification') %>%
+#   select(schoolQid = qid, carnegieQid = wikidataQid)
+# cat(paste(undergraduate.profile$schoolQid, 'P2643', undergraduate.profile$carnegieQid, 'S248','Q4223026',sep = '\t'), sep = '\n')
+
+undergraduate.program = merge(crosswalked, 
+                              carnegie.qid %>% filter(carnegieClassificationType == 'Undergraduate Program'),
+                              by.x = 'Undergraduate.Program', by.y = 'carnegieClassification') %>%
   select(schoolQid = qid, carnegieQid = wikidataQid)
-cat(paste(size.setting$schoolQid, 'P2643', size.setting$carnegieQid, 'S248','Q4223026',sep = '\t'), sep = '\n')
+cat(paste(undergraduate.program$schoolQid, 'P2643', undergraduate.program$carnegieQid, 'S248','Q4223026',sep = '\t'), sep = '\n')
